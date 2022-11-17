607e9d098969189609edd9721e02d9fdb0406689
angela <nik1955mix@gmail.com>
2022-11-17 21:40:41 +0300

    ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("введите трёхзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int result = (number % 100) / 10;
+
+Console.WriteLine($"{number} -> {result}");
